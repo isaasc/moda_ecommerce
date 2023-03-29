4be409582c35752b8ecf493cc0353255fa9accc5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-29 20:57:21 -0300

    adjusting

--- src/main/java/com/moda/ecommerce/controllers/CategoriaController.java
+++ src/main/java/com/moda/ecommerce/controllers/CategoriaController.java
@@ -2,12 +2,34 @@ package com.moda.ecommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.moda.ecommerce.models.Categoria;
 import com.moda.ecommerce.repositories.CategoriaRepository;
 
 @Controller
+@RequestMapping("/categoria")
 public class CategoriaController {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@GetMapping("")
+	public String getCategorias() {
+		return "create/createCategoria";
+	}
+	
+	@GetMapping("/index")
+	public String getIndex() {
+		return "index";
+	}
+	
+	@PostMapping("/create")
+	public String create(Categoria categoria) {
+		categoriaRepository.save(categoria);
+		return "redirect:/categoria/index";
+	}
 }
